1b843287f99bb9b03cb8a3c48fae75f03ff1b691
Alexeyzhu <alekseyzhuchkov.tut@gmail.com>
2018-06-02 22:23:35 +0400

    Added tests for queue

--- ha_2/tests/test_queue.c
+++ ha_2/tests/test_queue.c
@@ -3,41 +3,49 @@
 
 START_TEST (test_insert)
     {
+        //test with ordered elements
         for (int i = 0; i < 100; i++) {
             ck_assert_int_eq(insert(i * 0.1, i), 0);
         }
 
+        // test for max number of elements
         ck_assert_int_eq(insert(101.0, 1), 1);
 
         for (int i = 0; i < 100; i++) {
             ck_assert_double_eq(extract_min(), i * 0.1);
         }
 
+        // test with reversed ordered elements
         for (int i = 100; i > 0; i--) {
             ck_assert_int_eq(insert(i * 0.1, i), 0);
         }
 
+        // test for max number of elements
         ck_assert_int_eq(insert(0.1, 1), 1);
 
         for (int i = 1; i <= 100; i++) {
             ck_assert_double_eq(extract_min(), i * 0.1);
         }
 
-        insert(5.5, 5);
+        // test for stability
+        insert(5.1, 5);
         insert(1.1, 1);
-        insert(2.2, 2);
-        insert(3.3, 3);
-        insert(1.2, 1);
-        insert(4.4, 4);
+        insert(2.1, 2);
         insert(3.1, 3);
+        insert(1.2, 1);
+        insert(4.1, 4);
+        insert(3.2, 3);
         insert(1.3, 1);
         insert(1.4, 1);
-
-        ck_assert_double_eq(extract_min(), 7.7);
-        ck_assert_double_eq(extract_min(), 3.3);
-        ck_assert_double_eq(extract_min(), 5.5);
-        ck_assert_double_eq(extract_min(), 9.9);
         ck_assert_double_eq(extract_min(), 1.1);
+        ck_assert_double_eq(extract_min(), 1.2);
+        ck_assert_double_eq(extract_min(), 1.3);
+        ck_assert_double_eq(extract_min(), 1.4);
+        ck_assert_double_eq(extract_min(), 2.1);
+        ck_assert_double_eq(extract_min(), 3.1);
+        ck_assert_double_eq(extract_min(), 3.2);
+        ck_assert_double_eq(extract_min(), 4.1);
+        ck_assert_double_eq(extract_min(), 5.1);
 
 
     }
@@ -50,12 +58,16 @@ START_TEST (test_min)
         insert(3.3, 2);
         insert(5.5, 3);
         insert(7.7, 1);
+        insert(11.1, 6);
+        insert(13.3, 7);
         insert(1.1, 5);
         ck_assert_double_eq(extract_min(), 7.7);
         ck_assert_double_eq(extract_min(), 3.3);
         ck_assert_double_eq(extract_min(), 5.5);
         ck_assert_double_eq(extract_min(), 9.9);
         ck_assert_double_eq(extract_min(), 1.1);
+        ck_assert_double_eq(extract_min(), 11.1);
+        ck_assert_double_eq(extract_min(), 13.3);
         ck_assert_double_eq(extract_min(), -INFINITY);
     }
 END_TEST
