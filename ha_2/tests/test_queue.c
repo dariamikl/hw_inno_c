ac7f5fe7e815bc339c8b36df7e3acbcdcb488bf8
Alexey <alekseyzhuchkov.tut@gmail.com>
2018-06-02 20:55:02 +0400

    Update test_queue.c

--- ha_2/tests/test_queue.c
+++ ha_2/tests/test_queue.c
@@ -3,13 +3,32 @@
 
 START_TEST (test_insert)
     {
-        //YOUR CODE HERE
+        for (int i = 0; i < 100; i++) {
+            ck_assert_int_eq(insert(i * 0.1, i), 0);
+        }
+
+        ck_assert_int_eq(insert(101.0, 1), 1);
+
+        for (int i = 0; i < 100; i++) {
+            ck_assert_double_eq(extract_min(), i * 0.1);
+        }
     }
 END_TEST
 
 START_TEST (test_min)
     {
-        //YOUR CODE HERE
+        ck_assert_double_eq(extract_min(), -INFINITY);
+        insert(9.9, 4);
+        insert(3.3, 2);
+        insert(5.5, 3);
+        insert(7.7, 1);
+        insert(1.1, 5);
+        ck_assert_double_eq(extract_min(), 7.7);
+        ck_assert_double_eq(extract_min(), 3.3);
+        ck_assert_double_eq(extract_min(), 5.5);
+        ck_assert_double_eq(extract_min(), 9.9);
+        ck_assert_double_eq(extract_min(), 1.1);
+        ck_assert_double_eq(extract_min(), -INFINITY);
     }
 END_TEST
 
